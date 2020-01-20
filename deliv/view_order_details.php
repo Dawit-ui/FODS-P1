b397b7b4621898f00cdceef114a7ea93d06b9830
Dawit Dereje <dawitdereje260@gmail.com>
2020-01-20 14:25:14 +0300

    delivery-man portal

---
+++ deliv/view_order_details.php
@@ -0,0 +1,104 @@
+
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title> MY KITCHEN </title>
+  </head>
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+   
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a class="navbar-brand" href="index.php">MY KITCHEN</a>
+        </div>
+
+
+      </div>
+    </nav>
+
+
+
+
+<div class="container">
+    <div class="jumbotron">
+     <h1>Hello Delivery Man! </h1>
+     <p>All your Orders are here</p>
+
+    </div>
+    </div>
+
+<div class="container">
+    
+    	<div class="col-xs-3" style="text-align: center;">
+
+    	<div class="list-group">
+    		
+        <a href="view_order_details.php" class="list-group-item active">View Orders</a>
+    	</div>
+    </div>
+    
+
+
+    
+    <div class="col-xs-9">
+      <div class="form-area" style="padding: 0px 100px 100px 100px;">
+        <form action="" method="POST">
+        <br style="clear: both">
+          <h3 style="margin-bottom: 25px; text-align: center; font-size: 30px;"> YOUR FOOD ORDER LIST </h3>
+
+
+
+
+  <table class="table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th>  </th>
+        <th> Order ID </th>
+        <th> Food ID </th>
+        <th> Order Date </th>
+        <th> Food Name </th>
+        <th> Price </th>
+        <th> Quantity </th>
+        <th> Customer </th>
+      </tr>
+    </thead>
+
+   
+
+  
+  </table>
+    <br>
+
+
+  
+
+  <h4><center>0 RESULTS</center> </h4>
+
+ 
+
+        </form>
+
+        
+        </div>
+      
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+  
+  </body>
+</html>

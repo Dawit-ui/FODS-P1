f769878b9ac6d172353ef1add658bd8d789778cb
Dawit Dereje <dawitdereje260@gmail.com>
2020-01-22 08:06:40 +0300

    delivery panel with login

---
+++ deliv2/managerlogin.php
@@ -0,0 +1,121 @@
+    <?php
+include('login_m.php'); // Includes Login Script
+
+if(isset($_SESSION['login_user1'])){
+header("location: view_order_details.php"); //Redirecting to myrestaurant Page
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>MY KITCHEN</title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/managerlogin.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  
+  
+  
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">MY KITCHEN</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              
+
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+ 
+            </ul>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="container">
+    <div class="jumbotron">
+     <h1> Welcome to <span class="edit"> MY KITCHEN</span></h1>
+     <br>
+   <p>LOGIN to continue.</p>
+    </div>
+    </div>
+
+    <div class="container" style="margin-top: 4%; margin-bottom: 2%;">
+      <div class="col-md-5 col-md-offset-4">
+        <label style="margin-left: 5px;color: red;"><span> <?php echo $error;  ?> </span></label>
+      <div class="panel panel-primary">
+        <div class="panel-heading"> Login </div>
+        <div class="panel-body">
+          
+        <form action="" method="POST">
+          
+        <div class="row">
+          <div class="form-group col-xs-12">
+            <label for="username"><span class="text-danger" style="margin-right: 5px;">*</span> Username: </label>
+            <div class="input-group">
+              <input class="form-control" id="username" type="text" name="username" placeholder="Username" required="" autofocus="">
+              <span class="input-group-btn">
+                <label class="btn btn-primary"><span class="glyphicon glyphicon-user" aria-hidden="true"></label>
+            </span>
+              </span>
+            </div>           
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="form-group col-xs-12">
+            <label for="password"><span class="text-danger" style="margin-right: 5px;">*</span> Password: </label>
+            <div class="input-group">
+              <input class="form-control" id="password" type="password" name="password" placeholder="Password" required="">
+              <span class="input-group-btn">
+                <label class="btn btn-primary"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></label>
+            </span>
+              
+            </div>           
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="form-group col-xs-4">
+              <button class="btn btn-primary" name="submit" type="submit" value=" Login ">Submit</button>
+
+          </div>
+
+        </div>
+        <label style="margin-left: 5px;">or</label> <br>
+       <label style="margin-left: 5px;"><a href="managersignup.php">Create a new account.</a></label>
+       
+
+        </form>
+
+        </div>
+        
+      </div>
+      
+    </div>
+    </div>
+    </body>
+</html>
